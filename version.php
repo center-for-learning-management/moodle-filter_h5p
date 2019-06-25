a73870d746185a7e395af0ac50877cf877c7eef5
Robert Schrenk <robert@schrenk.cc>
2019-06-25 07:18:07 +0200

    Removed border from iFrame

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2019013002;
+$plugin->version  = 2019062500;
 $plugin->requires = 2014051200;  // Requires Moodle 2.7.
 $plugin->component = 'filter_h5p';
-$plugin->release = '1.0 (Build: 2019013002)';
+$plugin->release = '1.1 (Build: 2019062500)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'mod_hvp' => 2017031300
